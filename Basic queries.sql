ad238a696bb5c1ad7d4f0d1843eb157e36e9c09a
Kennedy Odongo <54551893+KennedyOdongo@users.noreply.github.com>
2024-02-13 09:51:57 -0600

    Always add semi-colons to your queries. Graded HW please merge pull request.

--- Basic queries.sql
+++ Basic queries.sql
@@ -1,3 +1,5 @@
+-- Always add a semi-colon at the end of your queries. Otherwise your code breaks and won't run 
+
 -- BITS
 
 USE bits;
@@ -22,7 +24,7 @@ WHERE type = 'Hiking' AND distance > 6;
 -- Q 4
 SELECT tripname
 FROM trip
-WHERE type = 'paddling' OR state = 'VT'
+WHERE type = 'paddling' OR state = 'VT';
 
 USE Sports;
 
@@ -33,17 +35,17 @@ FROM therapies;
 -- Q 3
 SELECT lastname, firstname
 FROM therapist 
-WHERE city = 'palm rivers'
+WHERE city = 'palm rivers';
 
 -- Q 4
 SELECT lastname, firstname
 FROM therapist 
-WHERE city <> 'palm rivers'
+WHERE city <> 'palm rivers';
 
 -- Q 5
 SELECT patientnum, lastname, firstname 
 FROM patient
-WHERE balance >= 3000
+WHERE balance >= 3000;
 
 -- Q 11
 SELECT patientnum, lastname
@@ -55,7 +57,7 @@ WHERE city IN ('palm rivers' , 'waterville' , 'munster') ;
 -- THIS COMMAND WOULD GIVE YOU THE SAME RESULT
 SELECT patientnum, lastname
 FROM patient
-WHERE city = 'palm rivers' OR city = 'waterville' OR city = 'munster'
+WHERE city = 'palm rivers' OR city = 'waterville' OR city = 'munster';
 
 -- Q 22
 SELECT *
